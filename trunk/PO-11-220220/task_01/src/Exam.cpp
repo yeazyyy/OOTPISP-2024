904f640dd67299a6332cefede2f6a67fdc425f45
DOXECEES <90423472+DOXECEES@users.noreply.github.com>
2024-03-07 09:17:28 +0300

    [task_01] Add task_01

---
+++ trunk/PO-11-220220/task_01/src/Exam.cpp
@@ -0,0 +1,65 @@
+#include "Exam.h"
+
+Exam::Exam(const Exam &other)
+{
+	this->date = other.date;
+
+	this->grade = other.grade;
+
+	this->studentName = new char[strlen(other.studentName) + 1];
+	strcpy_s(this->studentName, strlen(other.studentName) + 1, other.studentName);
+
+	std::cout << "Вызван конструктор копирования " << this << std::endl;
+}
+
+void Exam::SetName(const char *name)
+{
+	if (studentName != nullptr)
+		delete[] this->studentName;
+
+	this->studentName = new char[strlen(name) + 1];
+	strcpy_s(this->studentName, strlen(name) + 1, name);
+}
+
+void Exam::Set(const char *name, const int date, const int grade)
+{
+	this->date = date;
+	this->grade = grade;
+
+	if (studentName != nullptr)
+		delete[] this->studentName;
+
+	this->studentName = new char[strlen(name) + 1];
+	strcpy_s(this->studentName, strlen(name) + 1, name);
+}
+
+Exam::Exam(const char *name, const int date, const int grade)
+{
+	std::cout << "Вызван конструктор с параметрами " << this << std::endl;
+
+	this->studentName = new char[strlen(name) + 1];
+	strcpy_s(this->studentName, strlen(name) + 1, name);
+
+	this->date = date;
+	this->grade = grade;
+}
+
+Exam::Exam()
+	: studentName(nullptr), date(1), grade(0)
+{
+	std::cout << "Вызван конструктор без параметров " << this << std::endl;
+}
+
+Exam::~Exam()
+{
+	std::cout << "Вызван деструктор " << this << std::endl;
+	delete[] studentName;
+}
+
+void Exam::Print()
+{
+	std::cout << "Имя студента: " << studentName << std::endl;
+	std::cout << "День экзамена: " << date << std::endl;
+	std::cout << "Оценка: " << grade << std::endl;
+	std::cout << std::endl;
+}
